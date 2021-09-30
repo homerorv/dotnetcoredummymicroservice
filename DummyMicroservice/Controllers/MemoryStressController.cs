6a0d6f56dcb2fafab02681c5ced852cf27d6930f
hr101392 <homerorv@gmail.com>
2021-09-30 12:32:19 -0500

    Initial development

---
+++ DummyMicroservice/Controllers/MemoryStressController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DummyMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemoryStressController : ControllerBase
+    {
+
+        static byte[] _memoryholder = new byte[10];
+
+        [HttpGet]
+        public double Get(long megaBytes,bool release)
+        {
+            double mem = 0;
+            mem = StressMemory(megaBytes);
+            if(release == true)
+            {
+                mem = ReleaseMemory();
+            }
+            return mem;
+        }
+
+        static double StressMemory(long megaBytes)
+        {
+
+            if (megaBytes > 0)
+            {
+                _memoryholder = new byte[megaBytes * 1024 * 1024];  //dummy allocation of memory
+            }
+            var memInBytes = GC.GetTotalMemory(false);
+
+            var menInMega = ConvertBytesToMegabytes(memInBytes);
+
+            return menInMega;
+
+        }
+
+        static double ReleaseMemory()
+        {
+             _memoryholder = new byte[0];
+
+            GC.Collect();
+            var memInBytes = GC.GetTotalMemory(true);
+            var menInMega = ConvertBytesToMegabytes(memInBytes);
+
+            return menInMega;
+        }
+
+
+        static double ConvertBytesToMegabytes(long bytes)
+        {
+            return (bytes / 1024f) / 1024f;
+        }
+
+    }
+}
---
+++ DummyMicroservice/Startup.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DummyMicroservice
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHealthChecks()
+                .AddMemoryHealthCheck("memory");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+                {
+                    ResponseWriter = WriteResponse
+                });
+            });
+        }
+
+        private static Task WriteResponse(HttpContext context, HealthReport result)
+        {
+            context.Response.ContentType = "application/json";
+
+            var json = new JObject(
+                new JProperty("status", result.Status.ToString()),
+                new JProperty("results", new JObject(result.Entries.Select(pair =>
+                    new JProperty(pair.Key, new JObject(
+                        new JProperty("status", pair.Value.Status.ToString()),
+                        new JProperty("description", pair.Value.Description),
+                        new JProperty("data", new JObject(pair.Value.Data.Select(
+                            p => new JProperty(p.Key, p.Value))))))))));
+
+            return context.Response.WriteAsync(
+                json.ToString(Formatting.Indented));
+        }
+
+
+    }
+}
